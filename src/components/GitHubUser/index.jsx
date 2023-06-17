04dd2d3baecd087853bd57cdbbad47121971b4e9
luis <luisangel970930@gmail.com>
2023-06-17 00:12:01 -0400

    added components User

--- src/components/GitHubUser/index.jsx
+++ src/components/GitHubUser/index.jsx
@@ -1,18 +1,25 @@
 import GitHubLogo from "../GitHubLogo";
 import Loading from "../Loading";
 import Alert from "../Alert";
+import User from "../User";
 import GitHubUserSearch from "../GitHubUserSearch";
 import useSearch from "../../hooks/useSearch";
 
 function GitHubUser() {
   const { loading, detailUser, error, requestUser } = useSearch();
-  console.log(error);
+
   return (
     <>
       <GitHubLogo />
       <GitHubUserSearch onSubmit={requestUser} />
       {loading && <Loading />}
       {error && !loading && !detailUser.user && <Alert status={error} />}
+      {!loading && detailUser && !error && (
+        <>
+          {" "}
+          <User details={detailUser.user} />
+        </>
+      )}
     </>
   );
 }
